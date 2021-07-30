6cf19c7822ba993ead6a1bbf36084263c843b330
Leon <leongrinshpun@gmail.com>
2021-07-30 19:56:15 +0300

    Initial commit

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] [Range(0, 50)] int poolSize = 5;
+    [SerializeField] [Range(0.1f, 30f)]float spawnTimer = 1f;
+
+    GameObject[] pool;
+
+    void Awake()
+    {
+        PopulatePoll();
+    }
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    void PopulatePoll()
+    {
+        pool = new GameObject[poolSize];
+
+        for (int i = 0; i < pool.Length; i++)
+        {
+            pool[i] = Instantiate(enemyPrefab, transform);
+            pool[i].SetActive(false);
+        }
+    }
+
+    void EnableObjectInPool()
+    {
+        foreach(GameObject i in pool)
+        {
+            if (!i.activeInHierarchy)
+            {
+                i.SetActive(true);
+                return;
+            }
+        }
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            EnableObjectInPool();
+            yield return new WaitForSeconds(spawnTimer);
+        }
+    }
+}
